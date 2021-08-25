c0a851b9c0b07ba7fddd8932b216f44894a3d0f2
Tymofii Fesenko <tymofii.fesenko1999@gmail.com>
2021-08-25 10:11:41 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.bcc0ff13340b68fab1485c6e25f2f732.js"
 );
 
 self.addEventListener('message', (event) => {
